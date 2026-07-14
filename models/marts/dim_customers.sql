ce6790902439e0dbf6a2b6ca17f0ed16a0830f8c
Matthew Akrigg <matthewakrigg05@gmail.com>
2026-07-14 21:30:13 +0100

    added two staging models, refactored dim_customers and established materialisation strategy

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,23 +1,20 @@
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
+    SELECT 
+        *
+    
+    FROM
+        {{ ref('stg__jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
+    SELECT 
+        *
+    
+    FROM
+        {{ ref('stg__jaffle_shop__orders') }}
 
 ),
 
